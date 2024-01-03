96de3290da76167f31713c2424514154e853fa87
senyoudev <younesmeskafe2018@gmail.com>
2024-01-03 23:55:07 +0100

    variable scoope

--- ownership_part/src/main.rs
+++ ownership_part/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
-}
+    //ownership part
+        // variable scope
+    let s = String::from("hello");
+    // s is valid from this point forward
+    // do stuff with s
+    println!("{}", s);
+
+} // this scope is now over, and s is no longer valid
