116763c3afbff6861d4ba1dd66e4081ef1d1f8ac
senyoudev <younesmeskafe2018@gmail.com>
2024-01-04 22:13:56 +0100

    concise control flow with if let

--- enums_pattern_matching/src/main.rs
+++ enums_pattern_matching/src/main.rs
@@ -35,6 +35,12 @@ fn main() {
         _ => (),
     }
 
+    // Consise control flow with if let
+    let some_u8_value = Some(0u8);
+    if let Some(3) = some_u8_value {
+        println!("Three");
+    }
+    
     
 }
 
