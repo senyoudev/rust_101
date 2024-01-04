7234a166cd8d31325d2bd4ef0e0038b7ddd44787
senyoudev <younesmeskafe2018@gmail.com>
2024-01-04 18:00:26 +0100

    remove target

--- ownership_part/target/debug/deps/ownership_part-1aea6f90c4736e96.d
+++
@@ -1,5 +0,0 @@
-/Users/younesmeskafe/Desktop/rust101/ownership_part/target/debug/deps/ownership_part-1aea6f90c4736e96: src/main.rs
-
-/Users/younesmeskafe/Desktop/rust101/ownership_part/target/debug/deps/ownership_part-1aea6f90c4736e96.d: src/main.rs
-
-src/main.rs:
--- ownership_part/target/debug/ownership_part.d
+++
@@ -1,1 +0,0 @@
-/Users/younesmeskafe/Desktop/rust101/ownership_part/target/debug/ownership_part: /Users/younesmeskafe/Desktop/rust101/ownership_part/src/main.rs
