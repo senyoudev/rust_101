5ce655b0154920c388e780b2eb6b0cace4365e23
senyoudev <younesmeskafe2018@gmail.com>
2024-01-04 21:33:20 +0100

    match

--- enums_pattern_matching/src/main.rs
+++ enums_pattern_matching/src/main.rs
@@ -27,6 +27,15 @@ fn main() {
         3 => println!("Three"),
         _ => println!("Something else"),
     }
+
+    //Matching with Option<T>
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("Three"),
+        _ => (),
+    }
+
+    
 }
 
 //Matching with Option<T>
