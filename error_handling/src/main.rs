36edd1425e8a4e0a36beff4450e38dbdea20d5f9
senyoudev <younesmeskafe2018@gmail.com>
2024-01-05 15:40:20 +0100

    error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // we have two types of errors in rust : recoverable and unrecoverable
+    // recoverable errors are handled using Result<T, E> enum
+    // unrecoverable errors are handled using panic! macro
+
+    // 1 - 
+}
