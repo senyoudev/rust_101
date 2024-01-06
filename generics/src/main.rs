0a0b244f5856bd55ec06f6de10a7961016805759
senyoudev <younesmeskafe2018@gmail.com>
2024-01-06 21:42:17 +0100

    traits

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,6 +9,11 @@
 //     largest
 // }
 
+// Define Display trait
+use std::fmt::Display;
+// define PartialOrd trait
+use std::cmp::PartialOrd;
+
 // In struct definitions
 struct Point<T> {
     x: T,
@@ -29,6 +34,60 @@ impl<T> Point<T> {
 }
 
 
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+//Implementing a trait 
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+//Implementing a trait
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+// Defining a trait
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+
+// Pair struct with generic type parameters
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+
+
+// Conditionally implement methods on a generic type depending on trait bounds
+impl <T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+
 fn main() {
     // Generics in Rust
     // Generics are abstract stand-ins for concrete types or other properties.
@@ -44,6 +103,22 @@ fn main() {
     println!("p.x = {}", point.x()); // 5
 
         
+    // Traits: Defining Shared Behavior
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        content: String::from("The Pittsburgh Penguins once again are the best"),
+        author: String::from("Iceburgh"),
+    };
+    println!("New article available! {}", article.summarize());
 
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 
 }
