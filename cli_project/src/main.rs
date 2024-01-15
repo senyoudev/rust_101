bd45049f1566d2482fd92e2be27d37624854bcd5
senyoudev <younesmeskafe2018@gmail.com>
2024-01-15 23:33:00 +0100

    accepting args and reading file

--- cli_project/src/main.rs
+++ cli_project/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect(); // this line for command line arguments
+
+    if args.len() != 3 {
+        println!("Wrong number of arguments");
+        std::process::exit(1);
+    }
+
+    let query = &args[1];
+    let filename = &args[2];
+    
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    
+    let contents = fs::read_to_string(filename)
+        .expect("Something Went Wrong");
+
+    println!("With text:\n{}", contents);
 }
