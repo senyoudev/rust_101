2f9ea18b313a29f56e9673682e6f74e8f6c56a3c
senyoudev <younesmeskafe2018@gmail.com>
2024-01-16 00:03:18 +0100

    start refactoring

--- cli_project/src/main.rs
+++ cli_project/src/main.rs
@@ -1,22 +1,38 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+
+        if args.len() != 3 {
+            println!("Wrong number of arguments");
+            std::process::exit(1);
+        }
+    
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
+    }
+}
+
 fn main() {
     let args : Vec<String> = env::args().collect(); // this line for command line arguments
 
-    if args.len() != 3 {
-        println!("Wrong number of arguments");
-        std::process::exit(1);
-    }
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
     
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
     
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something Went Wrong");
 
     println!("With text:\n{}", contents);
 }
+
+
