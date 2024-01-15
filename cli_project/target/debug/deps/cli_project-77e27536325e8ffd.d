cd1f5b721db8b01327265b3fa43ed613cc53d31a
senyoudev <younesmeskafe2018@gmail.com>
2024-01-15 23:02:27 +0100

    error handling && cli project init

---
+++ cli_project/target/debug/deps/cli_project-77e27536325e8ffd.d
@@ -0,0 +1,5 @@
+/Users/younesmeskafe/Desktop/rust101/cli_project/target/debug/deps/libcli_project-77e27536325e8ffd.rmeta: src/main.rs
+
+/Users/younesmeskafe/Desktop/rust101/cli_project/target/debug/deps/cli_project-77e27536325e8ffd.d: src/main.rs
+
+src/main.rs:
---
+++ cli_project/target/debug/deps/cli_project-b8d802cdb9049512.d
@@ -0,0 +1,5 @@
+/Users/younesmeskafe/Desktop/rust101/cli_project/target/debug/deps/libcli_project-b8d802cdb9049512.rmeta: src/main.rs
+
+/Users/younesmeskafe/Desktop/rust101/cli_project/target/debug/deps/cli_project-b8d802cdb9049512.d: src/main.rs
+
+src/main.rs:
