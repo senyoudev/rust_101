fd98d25820e850c6808036fd0a5deaeae33da3d5
senyoudev <younesmeskafe2018@gmail.com>
2024-01-20 00:30:59 +0100

    writing errors to standard error

--- cli_project/src/main.rs
+++ cli_project/src/main.rs
@@ -9,14 +9,14 @@ fn main() {
 
 
     let config = Config::new(&args).unwrap_or_else(|err | {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     if let Err(e) = cli_project::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
    
