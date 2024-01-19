fc8fc82e626952fe6429d330be033e92c8f2588d
senyoudev <younesmeskafe2018@gmail.com>
2024-01-19 22:47:09 +0100

    refactoring

--- cli_project/src/main.rs
+++ cli_project/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,16 +8,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
 
-        if args.len() != 3 {
-            println!("Wrong number of arguments");
-            std::process::exit(1);
+        if args.len() < 3 {
+            return Err("Invalid Number of Arguments");
         }
     
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 }
 
@@ -24,7 +24,10 @@ fn main() {
     let args : Vec<String> = env::args().collect(); // this line for command line arguments
 
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err | {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
