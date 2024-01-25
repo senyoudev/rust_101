49f48d5a2015cc7ee947ced73e8cf8be898b6a8a
senyoudev <younesmeskafe2018@gmail.com>
2024-01-25 18:54:46 +0100

    closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/Users/younesmeskafe/Desktop/rust101/closures/target/debug/closures: /Users/younesmeskafe/Desktop/rust101/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-11e33943c9a24047.d
@@ -0,0 +1,5 @@
+/Users/younesmeskafe/Desktop/rust101/closures/target/debug/deps/closures-11e33943c9a24047: src/main.rs
+
+/Users/younesmeskafe/Desktop/rust101/closures/target/debug/deps/closures-11e33943c9a24047.d: src/main.rs
+
+src/main.rs:
